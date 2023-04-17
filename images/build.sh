821c40488d7361a1cb500b4974a463dc105784f1
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2023-04-17 16:54:30 +0800

    feat: add new testing environment: debian, ubuntu, opensuse

--- images/build.sh
+++ images/build.sh
@@ -2,5 +2,12 @@
 
 docker rmi -f localhost/ubi8-init
 docker rmi -f localhost/ubi9-init
+docker rmi -f localhost/debian-bullseye-systemd
+docker rmi -f localhost/ubuntu-20.04-systemd
+docker rmi -f localhost/opensuse-tumbleweed-systemd
+
 docker build -f ./Dockerfile.rhel8 -t localhost/ubi8-init .
 docker build -f ./Dockerfile.rhel9 -t localhost/ubi9-init .
+docker build -f ./Dockerfile.debian -t localhost/debian-bullseye-systemd .
+docker build -f ./Dockerfile.ubuntu -t localhost/ubuntu-20.04-systemd .
+docker build -f ./Dockerfile.opensuse -t localhost/opensuse-tumbleweed-systemd .
