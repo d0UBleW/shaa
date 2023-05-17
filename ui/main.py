4a95b9836afc4bb1b4b62b053c30230dccc880b4
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2023-05-17 16:39:09 +0800

    feat(ui): load inventory feature

--- ui/main.py
+++ ui/main.py
@@ -1,21 +1,54 @@
 #!/usr/bin/env python3
 
-from typing import Text
+from typing import Text, List
 import cmd
-from utils import utils
-import re
+from utils import inventory
+
+load_completions = [
+    "inventory",
+    "profile",
+    "preset",
+]
 
 
 class ShaaShell(cmd.Cmd):
     section = None
     profile = None
+    inventory = None
 
     @property
     def prompt(self):
-        prompt_str = ""
+        section_prompt = ""
         if self.section:
-            prompt_str = f"[{self.section}] "
-        return f"(shaa) {prompt_str}> "
+            section_prompt = f"[{self.section}] "
+
+        inventory_prompt = ""
+        if self.inventory:
+            inventory_prompt = f"[I: {self.inventory.name}] "
+
+        return f"\n{section_prompt}{inventory_prompt}\n(shaa) > "
+
+    def do_load(self, args: Text):
+        context, _, subargs = args.partition(' ')
+        if context == "inventory":
+            self.inventory = inventory.Inventory.load(subargs)
+            return
+        raise Exception("TODO: Not yet implemented")
+
+    def complete_load(self, text, line, begidx, endidx) -> List[Text]:
+        if begidx == 5:
+            return list(filter(lambda s: s.startswith(text), load_completions))
+
+        context = line.split()[1]
+        if context == "inventory":
+            inventories = inventory.list_inventory()
+            with open('/run/shm/abcd', 'w') as f:
+                f.write(f"{line} {begidx} {endidx}\n")
+                f.write(f"{' '.join(inventories)}\n")
+
+            return list(filter(lambda s: s.startswith(text), inventories))
+
+        return []
 
     def do_section(self, args):
         self.section = args
@@ -64,7 +97,7 @@ class ShaaShell(cmd.Cmd):
     def help_exit(self):
         print("Exit")
 
-    def complete_section(self, text, line, begidx, endidx):
+    def complete_section(self, text, line, begidx, endidx) -> List[Text]:
         return list(filter(lambda s: s.startswith(text), sections))
 
 
@@ -77,8 +110,7 @@ sections = [
 ]
 
 if __name__ == "__main__":
-    """ utils.foo() """
-    print("Welcome to SHAA shell. Type help or ? to list commands.\n")
+    print("Welcome to SHAA shell. Type help or ? to list commands.")
     shell = ShaaShell()
     while True:
         try:
