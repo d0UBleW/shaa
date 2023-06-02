ac11453d2012189c249ad4bf89e1b35bfbae4df4
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2023-06-02 20:49:33 +0800

    chore(shaa-shell): load env vars from package root

--- ui/shaa_shell/utils/vault.py
+++ ui/shaa_shell/utils/vault.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 from ansible_vault import Vault  # type: ignore[import]
 from dotenv import dotenv_values  # type: ignore[import]
+from shaa_shell.utils.path import ROOT_PATH
 
 try:
-    vault_password = dotenv_values(".env")["VAULT_PASSWORD"]
+    vault_password = dotenv_values(f"{ROOT_PATH}/.env")["VAULT_PASSWORD"]
 except KeyError:
     print("Missing ansible vault password!")
     print("Please set an environment variable named VAULT_PASSWORD via .env file")  # noqa: E501
