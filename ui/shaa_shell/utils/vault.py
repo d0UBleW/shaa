85583221ff6fe60440078edefdb93e76771eccc1
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2023-07-01 22:46:58 +0800

    feat(ui): alternative way to set VAULT_PASSWORD

--- ui/shaa_shell/utils/vault.py
+++ ui/shaa_shell/utils/vault.py
@@ -3,12 +3,22 @@ from __future__ import annotations
 from ansible_vault import Vault  # type: ignore[import]
 from dotenv import dotenv_values  # type: ignore[import]
 from shaa_shell.utils.path import ROOT_PATH
+import os
 
-try:
-    vault_password = dotenv_values(f"{ROOT_PATH}/.env")["VAULT_PASSWORD"]
-except KeyError:
+VAR_NAME = "VAULT_PASSWORD"
+dotenv_file = ROOT_PATH.joinpath(".env")
+
+if dotenv_file.is_file():
+    envs = dotenv_values(dotenv_file)
+else:
+    envs = os.environ  # type: ignore[assignment]
+
+if VAR_NAME not in envs.keys():
     print("[!] Missing ansible vault password!")
-    print("[!] Please set an environment variable named VAULT_PASSWORD via .env file")  # noqa: E501
+    print("[!] Please set an environment variable named VAULT_PASSWORD")
+    print(f"[!] This could be done by exporting or via {dotenv_file} file")
     exit(1)
+
+vault_password = envs[VAR_NAME]
 
 vault = Vault(vault_password)
