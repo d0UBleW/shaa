675872335467d4ddae702556a9bf5ed2c94c2cd2
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2023-07-03 22:46:38 +0800

    chore(docker): use alpine as the main one

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-IMAGE_NAME=shaa
+IMAGE_NAME="d0ublew/shaa"
 
 docker rmi -f "${IMAGE_NAME}"
-docker rmi -f "${IMAGE_NAME}-alpine"
 
-docker build -f ./Dockerfile -t "${IMAGE_NAME}" .
-docker build -f ./Dockerfile.alpine -t "${IMAGE_NAME}-alpine" .
+docker build -f ./Dockerfile.alpine -t "${IMAGE_NAME}" .
