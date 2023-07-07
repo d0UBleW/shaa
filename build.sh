58fd7c24da9ffb4fdc623592eee4933e3ca38716
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2023-07-07 09:14:16 +0800

    feat(docker): build with version tag

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 IMAGE_NAME="d0ublew/shaa"
+TAG="${1:-latest}"
 
-docker rmi -f "${IMAGE_NAME}"
+# docker rmi -f "${IMAGE_NAME}"
 
-docker build -f ./Dockerfile.alpine -t "${IMAGE_NAME}" .
+docker build -f ./Dockerfile.alpine -t "${IMAGE_NAME}:${TAG}" .
